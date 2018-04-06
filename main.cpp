1d7021b3d6c4379bab744408838d0268d4ec36f9
J <farleynick7@gmail.com>
2018-04-06 16:44:56 -0500

    Init
    Init-ed

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+//Begin!
+int main()
+{
+    //Words are stored here
+    vector<string> storage;
+
+    ifstream file;
+    file.open("words.txt");
+    
+    //Read in one word per line
+    while (!file.eof())
+    {
+        string tmp = "";
+        getline(file, tmp);
+        storage.push_back(tmp);
+        //To see it as it's read in
+        //cout << tmp << endl;
+    }
+    //cout << storage.size() << endl;
+
+    //Do something
+    cout << "Doing something" << endl;
+    for (int i = 0; i < storage.size(); i ++)
+    {
+        cout << storage[i] << endl; //Replace this with whatever
+    }
+    
+}
